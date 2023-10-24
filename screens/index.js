847be9e89202f5384412b223890e1ab9780fd059
novimonica <133090070+novimonica@users.noreply.github.com>
2023-10-24 20:54:43 +0700

    Add files via upload

---
+++ screens/index.js
@@ -0,0 +1,27 @@
+import LotsOfStyles from "./LotsOfStyles";
+import FixedDimensionsBasics from "./FixedDismensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasic";
+import PercentageDimensionsBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayout";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnimageWithStyle";
+import NativeBaseBasic from "./NativeBaseBasic";
+
+export {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+};
